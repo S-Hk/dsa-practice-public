4877f40c7e3a788e2e3e92def62f104b54d0cbbe
S-Hk (Hk_dev_) <38405687+S-Hk@users.noreply.github.com>
2025-11-02 01:30:33 +0530

    Day01_20251101_Array_check-if-array-is-sorted-and-rotated

---
+++ Arrays/Day01Nov25-ArraySortedRotated/LeetCode_D01_L1752.java
@@ -0,0 +1,36 @@
+/*
+Problem:
+https://leetcode.com/problems/merge-sorted-array/description/
+ */
+
+class Solution {
+    public boolean check(int[] nums) {
+        if(nums.length == 1){
+            return true;
+        }
+        int flipPos = -1;
+        for(int i = 0; i<nums.length-1; i++){
+            if(nums[i] > nums[i+1]){
+                if(flipPos != -1){
+                    return false;
+                }
+                flipPos = i+1;
+            }
+        }
+        if(flipPos == -1 && nums[nums.length-1]<nums[0]){
+            return false;
+        }
+        if(flipPos != -1 && nums[nums.length-1]>nums[0]){
+            return false;
+        }
+        return true;
+    }
+}
+/*
+Additional Test Cases:
+[5]
+[1,4,5,1]
+[1,1,1,1]
+[1,2,3,4,5,6,4,5]
+[9,1,5,7,8,9]
+*/
