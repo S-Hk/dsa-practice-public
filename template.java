4f2b1e8b7501bdbed4277eda68b680a3ef0b97dd
S-Hk (Hk_dev_) <38405687+S-Hk@users.noreply.github.com>
2025-11-03 01:26:07 +0530

    add template java file

---
+++ template.java
@@ -0,0 +1,20 @@
+/*
+Problem:
+<TODO: Paste URL here>
+ */
+
+class Solution {
+    public static void main(String args[]){
+        //TODO: replace with solutions
+    }
+}
+
+/*
+TODO: Any additional test cases tried
+*/
+
+
+//commit message format reference:
+/*
+Day00_20251100_Array_leetcode-url-path
+*/
