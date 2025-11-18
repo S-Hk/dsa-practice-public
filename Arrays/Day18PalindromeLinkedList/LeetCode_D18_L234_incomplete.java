e57296a8f62a054ea0679f6b99f913f732a6faab
S-Hk (Hk_dev_) <38405687+S-Hk@users.noreply.github.com>
2025-11-19 01:15:41 +0530

    Day18_20251118_List_palindrome-linked-list_unsolved-Incomplete-bugs

---
+++ Arrays/Day18PalindromeLinkedList/LeetCode_D18_L234_incomplete.java
@@ -0,0 +1,68 @@
+/*
+234. Palindrome Linked List
+https://leetcode.com/problems/palindrome-linked-list/
+ */
+
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+
+//code has logical flaws
+class Solution {
+    public boolean isPalindrome(ListNode head) {
+        if(head==null  || head.next==null) return true;
+        //find middle
+        //1,2,<3>,2,1-odd
+        //1,<2>,2,1-even
+        ListNode mid  = head;
+        ListNode fast = head;
+        //boolean even = false;
+
+        while(fast.next!=null && fast.next.next!=null){
+            //even = even? false: true;
+            mid = mid.next;
+            fast = fast.next.next;            
+        }
+
+        //reverse after mid
+        ListNode node = mid.next;
+        ListNode prev = null;
+        while(node.next!=null){
+            ListNode temp = node.next;
+            node.next=prev;
+            prev = node;
+            node = temp;
+        }
+        mid.next = node;
+
+
+        node = head; //node1 
+        fast = mid.next; //node2
+        while(fast.next!=null){
+            if(node.val!=fast.val)
+                return false;
+            node = node.next;
+            fast = fast.next;
+
+        }
+        //if(even){
+        //}
+        return true;
+    }
+}
+
+/*
+//using stack -
+    //time-O(n)
+    //space-O(n)
+//most optimal -  needed  hints from the video on approach
+    //time-O(n)
+    //space-O(1)
+*/
